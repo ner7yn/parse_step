c96f998fcf0b54df3e0ae4115046532b82913e27
Turtton <turtton@MacBook-Air-Turtton.local>
2025-11-27 14:55:03 +0400

    fix

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import logging
 import json
 
 # Настройка логирования
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
 class GalileoskyServer:
     def __init__(self, host='0.0.0.0', port=8000):
